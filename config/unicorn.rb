595c171cef7478a6d1b55652c2fb862d418c250c
robertlis <robert.lis2@mail.dcu.ie>
2014-10-31 18:55:35 -0400

    fixed unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,7 +5,7 @@ preload_app true
 working_directory "/var/www/tunesheap/current"
 stderr_path "/var/www/tunesheap/current/log/unicorn_err.log"
 stdout_path "/var/www/tunesheap/current/log/unicorn_out.log"
-pid "/var/www/tunesheap/current/pids/unicorn.pid"
+#pid "/var/www/tunesheap/current/pids/unicorn.pid"
 listen "/tmp/unicorn.tunesheap.sock"
 
 before_fork do |server, worker|
